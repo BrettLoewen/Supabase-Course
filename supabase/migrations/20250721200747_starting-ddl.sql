08a6d72faa315a482cc542309769d6b401758716
Brett Loewen <brettzky007@gmail.com>
2025-07-22 14:29:31 -0500

    RLS migration

--- supabase/migrations/20250721200747_starting-ddl.sql
+++ supabase/migrations/20250721200747_starting-ddl.sql
@@ -9,3 +9,29 @@ create table user_profiles
     (username) > 3 and char_length
     (username) < 15)
 );
+
+    alter table user_profiles enable row level security;
+
+    CREATE POLICY "all can see" ON "public"."user_profiles"
+AS PERMISSIVE FOR
+    SELECT
+        TO public
+USING
+    (true);
+
+    CREATE POLICY "users can insert" ON "public"."user_profiles"
+AS PERMISSIVE FOR
+    INSERT
+TO public
+WITH CHECK (auth.uid() =
+    user_id);
+
+    CREATE POLICY "owners can update" ON "public"."user_profiles"
+AS PERMISSIVE FOR
+    UPDATE
+TO public
+USING (auth.uid()
+    =user_id)
+WITH CHECK
+    (auth.uid
+    ()=user_id);
